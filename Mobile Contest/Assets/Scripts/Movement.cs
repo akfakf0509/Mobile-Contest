7cb1a471310492dbd8b1c2c94bdb5a2979a4e687
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2019-05-18 00:24:50 +0900

    script made

---
+++ Mobile Contest/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController ch;
+        Vector2 xyz = new Vector2();
+
+        if (Input.GetKey(KeyCode.A))
+            ch.Move(xyz);
+    }
+}
