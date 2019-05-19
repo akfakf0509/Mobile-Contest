54aad7d63692ae722c7401db0dcf3844ff9207fc
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2019-05-19 22:39:11 +0900

    2019.5.19 10.39

--- Mobile Contest/Assets/Scripts/SkillManager.cs
+++ Mobile Contest/Assets/Scripts/SkillManager.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    public GameObject basic_skill;
+    public GameObject avo_skill;
+    public GameObject Ultimate_skill;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
