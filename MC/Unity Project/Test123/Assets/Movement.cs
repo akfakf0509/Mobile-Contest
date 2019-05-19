d77750971bde982fdb4ac4e2d9cca3287d2bb42b
XxCtrlZxX <jhs030526@users.noreply.github.com>
2019-05-19 13:12:25 +0900

    unitytest

---
+++ MC/Unity Project/Test123/Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 moveVelocity = new Vector3();
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            moveVelocity = transform.right;
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+            moveVelocity = new Vector3(-1, 0, 0);
+
+        transform.position += moveVelocity * speed * Time.deltaTime;
+    }
+}
