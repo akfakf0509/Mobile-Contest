bde2f4c97133020f7398538f6a6584ceba89f94d
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2019-05-22 18:37:46 +0900

    2019.5.22 6.37

---
+++ Mobile Contest/Assets/Scripts/BasicSkill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSkill : MonoBehaviour
+{
+    public float skill_speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("destorykill");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * skill_speed * Time.deltaTime);
+    }
+    IEnumerator destorykill()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(GameObject.Find("skill1"));
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        coll.gameObject.transform.GetChild(1).GetComponent<HealthManager>().health -= 10;
+        Destroy(gameObject);
+    }
+}
--- Mobile Contest/Assets/Scripts/SkillManager.cs
+++ Mobile Contest/Assets/Scripts/SkillManager.cs
@@ -16,6 +16,6 @@ public class SkillManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
