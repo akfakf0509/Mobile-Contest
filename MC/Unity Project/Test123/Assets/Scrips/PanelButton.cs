17c516c08ec31a66cc35026f97ad832500c19b47
XxCtrlZxX <jhs030526@users.noreply.github.com>
2019-05-30 23:11:07 +0900

    0530

---
+++ MC/Unity Project/Test123/Assets/Scrips/PanelButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelButton : MonoBehaviour
+{
+    public GameObject PauseWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseWindow.SetActive(false);
+    }
+
+    public void OpenClick()
+    {
+        PauseWindow.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseClick()
+    {
+        PauseWindow.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void HomeClick()
+    {
+        SceneManager.LoadScene("StageScene");
+        Time.timeScale = 0;
+    }
+}
