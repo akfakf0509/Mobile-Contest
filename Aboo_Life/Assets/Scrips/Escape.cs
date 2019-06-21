15f4cc5dfbe6457ee7ade437d9b84a02bffc8516
XxCtrlZxX <48352078+XxCtrlZxX@users.noreply.github.com>
2019-06-21 21:08:26 +0900

    0621

---
+++ Aboo_Life/Assets/Scrips/Escape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    void Update()
+    {
+        //if (Application.platform == RuntimePlatform.Android)
+        //    if (Input.GetKeyDown(KeyCode.Escape))
+        //    {
+        //        GameObject.Find("OUTPanel").GetComponent<PanelManager>().OpenClick();
+        //    }
+    }
+}
