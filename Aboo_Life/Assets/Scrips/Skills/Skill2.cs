f9e73c659f8f51b54281530bbebfdb58b6e44f65
XxCtrlZxX <48352078+XxCtrlZxX@users.noreply.github.com>
2019-06-21 21:40:59 +0900

    0621.

---
+++ Aboo_Life/Assets/Scrips/Skills/Skill2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill2 : SkillManager
+{
+    void Start()
+    {
+        Skill_Cool = 12f;
+    }
+}
