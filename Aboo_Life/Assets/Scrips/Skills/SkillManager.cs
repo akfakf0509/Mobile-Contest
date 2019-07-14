bbf7e66d0276921bfa10847b5ce445bf0b125876
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2019-07-14 14:04:22 +0900

    2019.07.14 2.04

---
+++ Aboo_Life/Assets/Scrips/Skills/SkillManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SkillManager : MonoBehaviour
+{
+    public Image img_Skill;
+    public Text SkillTxt;
+    public float Skill_Cool;
+
+    private bool isCan = true;
+
+    public void SkillStart(BaseEventData data)
+    {
+        if (isCan && Time.timeScale == 1)
+        {
+            StartCoroutine(CoolTime(this.Skill_Cool));
+        }
+    }
+
+    IEnumerator CoolTime(float cool)
+    {
+        isCan = false;
+        while (cool >= 0.0f)
+        {
+            cool -= Time.deltaTime;
+            img_Skill.fillAmount = (this.Skill_Cool - cool) / this.Skill_Cool;
+            SkillTxt.text = "" + Mathf.Round(cool * 10) / 10;
+            yield return new WaitForFixedUpdate();
+        }
+        SkillTxt.text = "";
+        isCan = true;
+    }
+}
